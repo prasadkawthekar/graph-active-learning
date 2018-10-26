c71044f0f372fd4e2dedd10eb3f61ab274a6c876
Prasad Kawthekar <prasadkawthekar1@gmail.com>
2018-10-26 15:41:25 -0700

    code cleanup

--- code/cartProd.m
+++
@@ -1,6 +0,0 @@
-function result = cartProd(sets)
-% Returns cartesian product of vectors in sets
-    c = cell(1, numel(sets));
-    [c{:}] = ndgrid( sets{:} );
-    result = cell2mat( cellfun(@(v)v(:), c, 'UniformOutput',false) );
-end
